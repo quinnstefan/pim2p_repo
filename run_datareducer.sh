76dd3f874d51439824e3b8304fafc6eabaf766dc
Quinn <quinnstefan@gwu.edu>
2024-08-07 13:32:22 -0700

    fixing shell script

--- run_datareducer.sh
+++ run_datareducer.sh
@@ -4,4 +4,4 @@ INPUTFILE=flattree_pim2p_090215.root
 INPUTTREE=gd_pimprotprotinc__B4_F4_T1_S4
 OUTPUTFILE=test_090215.root
 
-root -b -q 'analysis_pim2p_F4.C("'${INPUTFILE}'","'${INPUTTREE}'","'${OUTPUTFILE}'")'
+root -b -q 'datareducer.C("'${INPUTFILE}'","'${INPUTTREE}'","'${OUTPUTFILE}'")'
