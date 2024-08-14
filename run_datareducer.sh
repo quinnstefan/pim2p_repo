616a23e2e2a5edf9ec18e484a2ae097c012e178a
Quinn <quinnstefan@gwu.edu>
2024-08-14 11:49:32 -0700

    removed run_datareducer.sh from the repository

--- run_datareducer.sh
+++
@@ -1,6 +0,0 @@
-echo "What's the input file, input tree, and outputfile?"
-
-INPUTFILE=../flattree_pim2p_090215.root
-OUTPUTFILE=test_090215.root
-
-root -b -q 'datareducer.C("'${INPUTFILE}'","'${OUTPUTFILE}'")'
